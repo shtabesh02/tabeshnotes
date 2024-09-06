4f646a563f247123bf020cd5894246cc039b4974
shtabesh02 <shtabesh02@gmail.com>
2024-09-06 01:22:38 -0400

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,36 +1,54 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-// GET /api/restore-user
 const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+// const courseRouter = require('./courses.js');
+// const lessonRouter = require('./lessons.js');
+// const commentRouter = require('./comments.js');
+// const starrednotesRouter = require('./starrednotes.js');
+// const userprofileRouter = require('./userprofile.js');
+// const completedRouter = require('./completed.js');
+// const enrollmentRouter = require('./enrollment.js');
+// const searchResult = require('./search.js');
+
+
+
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
 
-// Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
+router.use('/users', usersRouter);
+// router.use('/courses', courseRouter);
+// router.use('/lessons', lessonRouter);
+// router.use('/comments', commentRouter);
+// router.use('/starrednotes', starrednotesRouter);
+// router.use('/userprofile', userprofileRouter);
+// router.use('/completed', completedRouter);
+// router.use('/enrollment', enrollmentRouter);
+// router.use('/search', searchResult);
 
-  
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+
 
 
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+  
+
+
 
 
 
   // GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
     where: {
@@ -42,8 +60,22 @@ router.get('/set-token-cookie', async (_req, res) => {
 });
 
 
+
+
+// GET /api/restore-user
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+
+
 // GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
 router.get(
   '/require-auth',
   requireAuth,
@@ -51,6 +83,5 @@ router.get(
     return res.json(req.user);
   }
 );
-
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,29 @@
 // backend/routes/index.js
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
+
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 const apiRouter = require('./api');
 
+router.use(restoreUser);
 router.use('/api', apiRouter);
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+
+  router.use('/session', sessionRouter);
+  
+  router.use('/users', usersRouter);
+  
+  router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+  });
+  
 
 // Test Route
 // router.get('/hello/world', function(req, res) {
